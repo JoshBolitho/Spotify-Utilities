c348c0f7004d86f0f3a3c3d3b583f83de2e8b4ce
JoshBolitho <jsjb369@gmail.com>
2023-02-19 17:13:45 +1300

    add secrets file

---
+++ Secrets.js
@@ -0,0 +1,7 @@
+const redirectUri = '';
+const clientId = '';
+const clientSecret = '';
+
+module.exports.redirectUri = redirectUri;
+module.exports.clientId = clientId;
+module.exports.clientSecret = clientSecret;
